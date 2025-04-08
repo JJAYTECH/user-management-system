14b08232b8c3d3838388ae244f869a519c5f3907
unknown <matthewrodan1@gmail.com>
2025-04-08 16:20:50 +0800

    Roldan-Frontend

--- src/app/account/forgot-password.component.ts
+++ src/app/account/forgot-password.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'x9;
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first, finalize } from 'rxjs/operators';
 
